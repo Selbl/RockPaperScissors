0c9c08c97c204d2ad96b449a2a20423b30976367
Stefan Elbl <selbl@DN0a23282b.SUNet>
2024-08-16 10:51:18 -0700

    Added UI changes

--- javascript.js
+++ javascript.js
@@ -26,28 +26,40 @@ function getHumanChoice() {
 }
 
 function playRound(){
+    const roundResultElem = document.querySelector("#roundResult");
     var computerChoice = getComputerChoice();
     var humanChoice = getHumanChoice();
     // Check cases
     if (humanChoice === computerChoice) {
-        console.log('Tie!');
+        roundResultElem.textContent = 'Tie!';
     }
     else if (match[humanChoice] === computerChoice){
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        roundResultElem.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+        //console.log(`You win! ${humanChoice} beats ${computerChoice}`);
         humanScore++;
     }
     else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        roundResultElem.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+        //console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
         computerScore++;
     }
-    console.log(`Current score, You: ${humanScore}, Computer: ${computerScore}`);
+    // Write score
+    const humanScoreElem = document.querySelector("#humanScore");
+    const computerScoreElem = document.querySelector("#computerScore");
+
+    humanScoreElem.textContent = `Current Human Score: ${humanScore}`;
+    computerScoreElem.textContent = `Current Computer Score: ${computerScore}`;
+    //console.log(`Current score, You: ${humanScore}, Computer: ${computerScore}`);
 }
 
 function playGame(){
     // Play rounds
+    /*
     for (let i = 0; i < 5; i++) {
         playRound();
     }
+    */
+    playRound();
     // Check score
     if (humanScore > computerScore) {
         console.log('You win!');
@@ -60,4 +72,30 @@ function playGame(){
     }
 }
 
-playGame();
+// track number of rounds
+var rounds = 0;
+const button = document.querySelector("#btnPlayRound");
+button.addEventListener("click", () => {
+    if (rounds < 5){
+        playRound();
+        rounds++;
+    }
+    // Check final score
+    if (rounds == 5){
+        const finalResultElem = document.querySelector("#finalResult");
+        // Check score
+        if (humanScore > computerScore) {
+            finalResultElem.textContent = `You win! Restart the browser to play again.`;
+        }
+        else if (humanScore < computerScore) {
+            finalResultElem.textContent = `You lose! Restart the browser to play again.`;
+        }
+        else{
+            finalResultElem.textContent = `Tie! Restart the browser to play again.`;;
+        }
+    }
+})
+
+//If number of rounds are equal to 5, stop the game!
+
+// playGame();
